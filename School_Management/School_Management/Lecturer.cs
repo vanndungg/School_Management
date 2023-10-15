1cb9fa3fdebbe50158277e767a8f6ba4fc1b1f31
Van Dung <vandungg152003@gmail.com>
2023-10-15 13:06:02 +0700

    source code

---
+++ School_Management/School_Management/Lecturer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management
+{
+    internal class Lecturer
+    {
+        public string lecId { get; set; }
+        public string lecName { get; set; }
+        public string lecDoB { get; set; }
+        public string lecEmail { get; set; }
+        public string lecAddress { get; set; }
+        public string lecDept { get; set; }
+
+        public Lecturer(string id, string name, string dob, string email, string address, string dept)
+        {
+            lecId = id;
+            lecName = name;
+            lecDoB = dob;
+            lecEmail = email;
+            lecAddress = address;
+            lecDept = dept;
+        }
+    }
+}
---
+++ School_Management/School_Management/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management
+{
+    internal class Student
+    {
+        public string stdId { get; set; }
+        public string stdName { get; set; }
+        public string stdDoB { get; set; }
+        public string stdEmail { get; set; }
+        public string stdAddress { get; set; }
+        public string stdClass { get; set; }
+
+        public Student(string id, string name, string dob, string email, string address, string _class)
+        {
+            stdId = id;
+            stdName = name;
+            stdDoB = dob;
+            stdEmail = email;
+            stdAddress = address;
+            stdClass = _class;
+        }
+    }
+}
